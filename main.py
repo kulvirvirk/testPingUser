745191f5969f06ffe03a7035fe4913286d6b860f
KVirkDev <kvirkdev@gmail.com>
2020-06-29 05:07:46 +0000

    - updated user_active requirements

--- main.py
+++ main.py
@@ -5,9 +5,11 @@
 
 
 # this "user_active = True" variable can be used to test both cases (if user is active or disabled - toggle true false values)
+# if user is active set:      user_active = True
+# if user is note active set: user_active = False
 user_active = True
 
-# this user_w_ping variable can be used to test both cases (w/ ping and w/o ping - toggle true false values)
+
 user_w_ping = True
 
 # this user_w_ping variable can be used to test both cases (w/ ping and w/o ping - toggle true false values)
